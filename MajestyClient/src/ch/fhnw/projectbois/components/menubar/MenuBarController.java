e4fd89e8db58c119c2a3e79edf072e1ce08d67d1
Dario Stöckli <dario.stoeckli@students.fhnw.ch>
2018-11-07 16:15:19 +0100

    Added Leaderboard Button to MenuBar

--- MajestyClient/src/ch/fhnw/projectbois/components/menubar/MenuBarController.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/menubar/MenuBarController.java
@@ -4,6 +4,9 @@ import ch.fhnw.projectbois._mvc.Controller;
 import ch.fhnw.projectbois.game.GameController;
 import ch.fhnw.projectbois.game.GameModel;
 import ch.fhnw.projectbois.game.GameView;
+import ch.fhnw.projectbois.leaderboard.LeaderboardController;
+import ch.fhnw.projectbois.leaderboard.LeaderboardModel;
+import ch.fhnw.projectbois.leaderboard.LeaderboardView;
 import ch.fhnw.projectbois.network.Network;
 import ch.fhnw.projectbois.playscreen.PlayScreenController;
 import ch.fhnw.projectbois.playscreen.PlayScreenModel;
@@ -33,11 +36,19 @@ public class MenuBarController extends Controller<MenuBarModel, MenuBarView> {
 		Network.getInstance().sendTest();
 	}
 
+	// Get to Lobby Screen where the player can either create a lobby or join one.
 	@FXML
 	private void btn_menubarview_play_view(ActionEvent event) {
 		PlayScreenController controller = Controller.initMVC(PlayScreenController.class, PlayScreenModel.class,
 				PlayScreenView.class);
 
+		this.switchCenter(controller.getViewRoot());
+	}
+	
+	// Get to the Leaderboard Screen 
+	@FXML
+	private void btn_menubarview_leaderboard_view(ActionEvent event) {
+		LeaderboardController controller = Controller.initMVC(LeaderboardController.class, LeaderboardModel.class, LeaderboardView.class);
 		this.switchCenter(controller.getViewRoot());
 	}
 
