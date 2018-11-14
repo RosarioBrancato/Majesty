b38fef763e7c94fc909b31e98acb37979d2f7123
Rosario B <Strather@users.noreply.github.com>
2018-11-14 23:11:18 +0100

    Game-Objects Added

--- MajestyClient/src/ch/fhnw/projectbois/game/GameView.java
+++ MajestyClient/src/ch/fhnw/projectbois/game/GameView.java
@@ -2,7 +2,13 @@ package ch.fhnw.projectbois.game;
 
 import java.net.URL;
 
+import ch.fhnw.projectbois._mvc.Controller;
 import ch.fhnw.projectbois._mvc.View;
+import ch.fhnw.projectbois.components.chat.ChatController;
+import ch.fhnw.projectbois.components.chat.ChatModel;
+import ch.fhnw.projectbois.components.chat.ChatView;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 
 public class GameView extends View<GameModel> {
 
@@ -13,6 +19,19 @@ public class GameView extends View<GameModel> {
 	@Override
 	protected URL getFXML() {
 		return this.getClass().getResource("GameView.fxml");
+	}
+	
+	@Override
+	public <T extends Controller<GameModel, ? extends View<GameModel>>> void loadRoot(T controller) {
+		super.loadRoot(controller);
+		
+		Parent root = this.root;
+		
+		BorderPane boarderPane = new BorderPane();
+		boarderPane.setCenter(root);
+		boarderPane.setRight(Controller.initMVC(ChatController.class, ChatModel.class, ChatView.class).getViewRoot());
+		
+		this.root = boarderPane;
 	}
 
 }
---
+++ MajestyCommons/src/ch/fhnw/projectbois/gameobjects/DisplayCard.java
@@ -0,0 +1,15 @@
+package ch.fhnw.projectbois.gameobjects;
+
+public class DisplayCard extends Card {
+
+	private int meeples = 0;
+
+	public int getMeeples() {
+		return meeples;
+	}
+
+	public void setMeeples(int meeples) {
+		this.meeples = meeples;
+	}
+	
+}
