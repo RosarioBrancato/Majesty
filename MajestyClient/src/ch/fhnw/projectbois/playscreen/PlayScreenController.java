749b066b713a77fe24cdb4381db85909e87b7dab
Dario Stöckli <dario.stoeckli@students.fhnw.ch>
2018-11-17 11:47:57 +0100

    PlayScreen Adjustments
    PlayScreen Adjustments

--- MajestyClient/src/ch/fhnw/projectbois/playscreen/PlayScreenController.java
+++ MajestyClient/src/ch/fhnw/projectbois/playscreen/PlayScreenController.java
@@ -1,8 +1,11 @@
 package ch.fhnw.projectbois.playscreen;
 
 import ch.fhnw.projectbois._mvc.Controller;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 /**
  * 
@@ -16,8 +19,37 @@ public class PlayScreenController extends Controller<PlayScreenModel, PlayScreen
 		super(model, view);
 	}
 	
+	
+	//Preparing List for GameMode ChoiceBox
+	ObservableList<String> gamemode_list=FXCollections.observableArrayList();
+	
+	private void loadData() {
+		//Loading Dropdown List for Hosting game
+		gamemode_list.removeAll(gamemode_list);
+		String a = "A";
+		String b = "B";
+		gamemode_list.addAll(a,b);
+		choicebx_playscreen_cardside.getItems().addAll(gamemode_list);
+		choicebx_playscreen_cardside.getSelectionModel().selectFirst();
+		//Loading Games to join
+		model.getLobbies();
+		// TO DO
+	}
+
+    @FXML
+    public void initialize() {
+        loadData();
+    }
+	
 	@FXML
-	public void btn_playscreen_start_test(ActionEvent e) {
+	private ChoiceBox<String> choicebx_playscreen_cardside;
+
+	@FXML
+	public void btn_playscreen_start(ActionEvent e) {
+	System.out.println(choicebx_playscreen_cardside.getValue());
+	String gamemode = choicebx_playscreen_cardside.getValue();
+	model.createLobby(gamemode);
+	
 		
 	}
 	
--- MajestyClient/src/ch/fhnw/projectbois/playscreen/PlayScreenModel.java
+++ MajestyClient/src/ch/fhnw/projectbois/playscreen/PlayScreenModel.java
@@ -1,6 +1,16 @@
 package ch.fhnw.projectbois.playscreen;
 
 import ch.fhnw.projectbois._mvc.Model;
+import ch.fhnw.projectbois.communication.Request;
+import ch.fhnw.projectbois.communication.RequestId;
+import ch.fhnw.projectbois.communication.Response;
+import ch.fhnw.projectbois.communication.ResponseId;
+import ch.fhnw.projectbois.gameobjects.GameState;
+import ch.fhnw.projectbois.json.JsonUtils;
+import ch.fhnw.projectbois.network.Network;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 /**
  * 
@@ -9,5 +19,37 @@ import ch.fhnw.projectbois._mvc.Model;
  */
 
 public class PlayScreenModel extends Model {
+	
+	public PlayScreenModel() {
+		this.initResponseListener();
+	}
+	
+	void createLobby(String gamemode) {
+		// do not forget to give the gamemode information
+		Request request = new Request("PLACEHOLDER TOKEN", RequestId.CREATE_LOBBY, null);
+		Network.getInstance().sendRequest(request);
+	}
+	
+	void getLobbies() {
+		Request request = new Request("PLACEHOLDER TOKEN", RequestId.GET_LOBBIES, null);
+		Network.getInstance().sendRequest(request);
+	}
+	
+	@Override
+	protected ChangeListener<Response> getChangeListener() {
+		return new ChangeListener<Response>() {
+			@Override
+			public void changed(ObservableValue<? extends Response> observable, Response oldValue, Response newValue) {
 
+				if (newValue.getResponse() == ResponseId.PLAYERS_LOBBY) {
+					String json = newValue.getJsonDataObject();
+					//GameState gameState = JsonUtils.Deserialize(json, GameState.class);
+
+					//gameStateProperty.setValue(gameState);
+					System.out.println("PlayScreenModel - Lobbies updated!");
+				}
+
+			}
+		};
+	}
 }
