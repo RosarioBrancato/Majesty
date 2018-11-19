8b2a31734ff64a5194ddfc0f4a9da8d4752b5fec
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-11-19 20:12:16 +0100

    Chat First Steps

--- MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatModel.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatModel.java
@@ -3,5 +3,29 @@ package ch.fhnw.projectbois.components.chat;
 import ch.fhnw.projectbois._mvc.Model;
 
 public class ChatModel extends Model {
+	
+	
+	public String message;
+	
+	public void sendMessage() {
+//		message = ChatModel.txtMessage.getText();
+		
+//		String json = JsonUtils.Serialize(message);
+//		Request request = new Request("XXX", RequestId.DO_MOVE, json);
+//		
+//		Network.getInstance().sendRequest(request);
+	}
+	
+	public void whisperMessage() {
+		
+	}
+	
+	public void postMessage() {
+		
+	}
+	
+	public void closeChatWindow() {
+		
+	}
 
 }
