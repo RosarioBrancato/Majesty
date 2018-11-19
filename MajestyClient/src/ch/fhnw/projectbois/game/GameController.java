9387584a681e72b8fc3d9511d000ea836566aba0
Rosario B <Strather@users.noreply.github.com>
2018-11-19 01:03:54 +0100

    Meeples and Location Card Count Added

--- MajestyClient/src/ch/fhnw/projectbois/game/GameController.java
+++ MajestyClient/src/ch/fhnw/projectbois/game/GameController.java
@@ -7,17 +7,28 @@ import ch.fhnw.projectbois.gameobjects.*;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 public class GameController extends Controller<GameModel, GameView> {
 
 	private GameResourceHelper resourceHelper = null;
-	
+
 	private int playerId = -1;
 	private GameState gameState = null;
+
+	private Label[] locationCardCount = null;
+
+	@FXML
+	private Label lblGameInfo;
+
+	@FXML
+	private Label lblPlayerInfo;
 
 	@FXML
 	private GridPane pnlDisplay;
@@ -51,23 +62,35 @@ public class GameController extends Controller<GameModel, GameView> {
 
 	private void initLocations() {
 		int sideChange = 0;
-		if(!this.gameState.isCardSideA()) {
+		if (!this.gameState.isCardSideA()) {
 			sideChange = 10;
 		}
-		
+
+		this.locationCardCount = new Label[8];
+
 		for (int i = 0; i < 8; i++) {
 			ImageView imgLocation = new ImageView();
 			imgLocation.setPreserveRatio(true);
-			imgLocation.setFitHeight(150);
-			
+			imgLocation.setFitHeight(175);
+
 			Image image = resourceHelper.getLocationImage(i + 1 + sideChange);
 			imgLocation.setImage(image);
 
+			BorderPane borderPane = new BorderPane(imgLocation);
+
+			this.locationCardCount[i] = new Label("Cards: 0");
+
+			VBox box = new VBox();
+			box.setAlignment(Pos.TOP_CENTER);
+			box.getChildren().add(this.locationCardCount[i]);
+			borderPane.setBottom(box);
+
 			Integer col = new Integer(i);
 			Platform.runLater(() -> {
-				this.pnlPlayer1Cards.add(imgLocation, col, 0);
+				this.pnlPlayer1Cards.add(borderPane, col, 0);
 			});
 		}
+		
 	}
 
 	private void loadGameState(GameState gameState) {
@@ -87,7 +110,27 @@ public class GameController extends Controller<GameModel, GameView> {
 	private void drawGui() {
 		Board board = this.gameState.getBoard();
 
+		this.loadGameInfos();
 		this.drawDisplay(board.getDisplay());
+	}
+
+	private void loadGameInfos() {
+		Board board = this.gameState.getBoard();
+
+		// Player currentPlayer = board.getPlayers().stream()
+		// .filter(f -> f.getUserToken() ==
+		// Session.getCurrentUserToken()).findFirst().get();
+
+		// TEMP
+		Player currentPlayer = board.getPlayers().get(0);
+
+		Platform.runLater(() -> {
+			this.lblPlayerInfo
+					.setText("Meeples: " + currentPlayer.getMeeples() + " Points: " + currentPlayer.getScore());
+
+			this.lblGameInfo.setText("Round: " + board.getRound() + "/12 Player's turn: " + board.getPlayersTurn());
+		});
+
 	}
 
 	private void drawDisplay(ArrayList<DisplayCard> displayCards) {
@@ -99,8 +142,8 @@ public class GameController extends Controller<GameModel, GameView> {
 		for (DisplayCard card : displayCards) {
 			ImageView imgCard = new ImageView();
 			imgCard.setPreserveRatio(true);
-			imgCard.setFitHeight(150);
-			
+			imgCard.setFitHeight(175);
+
 			Image image = resourceHelper.getCardImage(card);
 			imgCard.setImage(image);
 
@@ -112,12 +155,15 @@ public class GameController extends Controller<GameModel, GameView> {
 				}
 			});
 
-			BorderPane borderPane = new BorderPane(imgCard);
-			borderPane.getStyleClass().add("display");
+			VBox vbox = new VBox();
+			vbox.getStyleClass().add("display");
+			vbox.setAlignment(Pos.CENTER);
+			vbox.getChildren().add(imgCard);
+			vbox.getChildren().add(new Label("Meeples: " + card.getMeeples()));
 
 			Integer colIndex = new Integer(col);
 			Platform.runLater(() -> {
-				this.pnlDisplay.add(borderPane, colIndex, 0);
+				this.pnlDisplay.add(vbox, colIndex, 0);
 			});
 			col++;
 		}
@@ -125,16 +171,19 @@ public class GameController extends Controller<GameModel, GameView> {
 		// Deck
 		ImageView imgDeck = new ImageView();
 		imgDeck.setPreserveRatio(true);
-		imgDeck.setFitHeight(150);
+		imgDeck.setFitHeight(175);
 
 		int deckBack = this.gameState.getBoard().getDeckBack();
 		Image image = this.resourceHelper.getDeckBackImage(deckBack);
 		imgDeck.setImage(image);
 
-		BorderPane borderPane = new BorderPane(imgDeck);
+		VBox vbox = new VBox();
+		vbox.setAlignment(Pos.CENTER);
+		vbox.getChildren().add(imgDeck);
+		vbox.getChildren().add(new Label("Cards left: --"));
 
 		Platform.runLater(() -> {
-			this.pnlDisplay.add(borderPane, 6, 0);
+			this.pnlDisplay.add(vbox, 6, 0);
 		});
 	}
 
