0c410a0b7746a0590ae5039457279ff73bae7509
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-11-26 19:01:51 +0100

    second steps chat

--- MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatController.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatController.java
@@ -1,10 +1,23 @@
 package ch.fhnw.projectbois.components.chat;
 
+import ch.fhnw.projectbois._application.MetaContainer;
 import ch.fhnw.projectbois._mvc.Controller;
+import ch.fhnw.projectbois.dto.LobbyListDTO;
+import ch.fhnw.projectbois.dto.MessageDTO;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Modality;
+
+/**
+ * 
+ * @author Leeroy Koller
+ *
+ */
 
 public class ChatController extends Controller<ChatModel, ChatView> {
 	
@@ -12,6 +25,7 @@ public class ChatController extends Controller<ChatModel, ChatView> {
 		super(model, view);
 	}
 	
+	//declaration of things I need need in code
 	@FXML
 	private TextArea txtChat;
 	
@@ -26,13 +40,54 @@ public class ChatController extends Controller<ChatModel, ChatView> {
 	
 	@FXML
 	private void btnSend_Click(ActionEvent event) {
-		System.out.println("msg sent...");
+		//create object
+		MessageDTO message = new MessageDTO();
+		
+		//check if whispering -> do this here or in model??
+//		if (txtMessage.getText().matches(null){
+//			
+//		}
+		//get text from textarea and assign to object
+		message.setMessage(txtMessage.getText());
+		
+		model.getChatProperty().addListener((observer, oldValue, newValue) -> {
+			updateChatView(newValue);
+		});
 
-	}
+		model.getErrorProperty().addListener((observer, oldValue, newValue) -> {
+			Platform.runLater(() -> {
+				Alert dlg = new Alert(AlertType.ERROR);
+				dlg.setTitle("ERROR");
+				dlg.setHeaderText(null);
+				dlg.setContentText("An error occured. Please try again.");
+				
+				dlg.initOwner(MetaContainer.getInstance().getMainStage());
+				dlg.initModality(Modality.APPLICATION_MODAL);
+				
+				dlg.showAndWait();
+				
+				//getLobbies();
+			});
+		});
+		
+		
+		
+		
+		model.sendMessage(message);
+		
+		txtMessage.clear();
+		}
+	
 	
 	@FXML
 	private void btnMinimize_Click(ActionEvent event) {
 		this.getViewRoot().setVisible(false);
+	}
+	
+	private void updateChatView(MessageDTO post) {
+		Platform.runLater(() -> {
+			//txtChat.appendText(post);
+		});
 	}
 	
 }
--- MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatModel.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatModel.java
@@ -1,50 +1,81 @@
 package ch.fhnw.projectbois.components.chat;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import ch.fhnw.projectbois._mvc.Model;
+import ch.fhnw.projectbois.communication.Request;
+import ch.fhnw.projectbois.communication.RequestId;
+import ch.fhnw.projectbois.communication.Response;
+import ch.fhnw.projectbois.communication.ResponseId;
+import ch.fhnw.projectbois.dto.LobbyDTO;
+import ch.fhnw.projectbois.dto.LobbyListDTO;
+import ch.fhnw.projectbois.dto.MessageDTO;
+import ch.fhnw.projectbois.json.JsonUtils;
+import ch.fhnw.projectbois.network.Network;
+import ch.fhnw.projectbois.session.Session;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+/**
+ * 
+ * @author Leeroy Koller
+ *
+ */
 
 public class ChatModel extends Model {
 	
+	private SimpleObjectProperty<MessageDTO> chatProperty = null;
+	private SimpleObjectProperty<Timestamp> errorProperty = null;
 	
-	public String message;
 	
-	public void sendMessage() {
-	
-//		get text
-//		message = ChatModel.txtMessage.getText();   
-		
+	public void sendMessage(MessageDTO message) {
 //		create object
-//		String json = JsonUtils.Serialize(message);
+		String json = JsonUtils.Serialize(message);
 		
-//		create and send request
-//		Request request = new Request("XXX", RequestId.DO_MOVE, json);
-//		
-//		Network.getInstance().sendRequest(request);
-//      postMessage("Me",message);
+//		create and send request. UserToken: identify user, RequestId: knows which logic to use, json: has all the infos
+		Request request = new Request(Session.getCurrentUserToken(), RequestId.CHAT_SEND_MSG, json);
+		
+		Network.getInstance().sendRequest(request);
 	}
 	
 	public void whisperMessage() {
 
 	}
 	
-//	public void postMessage(sender, message) {
+	//receive response from server
+	@Override
+	protected ChangeListener<Response> getChangeListener() {
+		return new ChangeListener<Response>() {
+			@Override
+			public void changed(ObservableValue<? extends Response> observable, Response oldValue, Response newValue) {
+
+				if (newValue.getResponseId() == ResponseId.RECEIVE_MSG) {
+					String json = newValue.getJsonDataObject();
+					MessageDTO post = JsonUtils.Deserialize(json, MessageDTO.class);
+
+					postMessage(post);
+
+				} else if (newValue.getResponseId() == ResponseId.CHAT_ERROR) {
+					//errorProperty.setValue(new Timestamp(new Date().getTime()));
+				}
+
+			}
+		};
+	}
+	
+	public SimpleObjectProperty<MessageDTO> getChatProperty() {
+		return this.chatProperty;
+	}
+
+	public SimpleObjectProperty<Timestamp> getErrorProperty() {
+		return this.errorProperty;
+	}
+	
+	public void postMessage(MessageDTO post) { //until now only message. receiver and author?
 //		ChatController.txtChat.appendText(sender + ": " + message);
-//	}
-	
-	public void getMessageFromServer() {
-//		create request object
-		
-//		register call back
-		handleResponse();
-//		send request
-	}
-	
-	public void handleResponse() {
-//		check if reply is ok somehow...or do this on server side?
-		
-	}
-	
-	public void minimizeChatWindow() {
-		
+
 	}
 
 }
