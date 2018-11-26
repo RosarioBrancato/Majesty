e68bd5ba47a04ffa36d6d790d65966ccda78b351
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-11-26 19:01:51 +0100

    second steps chat

---
+++ MajestyCommons/src/ch/fhnw/projectbois/enumerations/ChatMember.java
@@ -0,0 +1,5 @@
+package ch.fhnw.projectbois.enumerations;
+
+public enum ChatMember {
+	Player1, Player2, Player3, Player4, All, System
+}
--- MajestyServer/src/ch/fhnw/projectbois/requesthandlers/ChatRequestHandler.java
+++ MajestyServer/src/ch/fhnw/projectbois/requesthandlers/ChatRequestHandler.java
@@ -2,9 +2,19 @@ package ch.fhnw.projectbois.requesthandlers;
 
 import ch.fhnw.projectbois.communication.Request;
 import ch.fhnw.projectbois.communication.RequestId;
+import ch.fhnw.projectbois.communication.Response;
+import ch.fhnw.projectbois.communication.ResponseId;
+import ch.fhnw.projectbois.dto.LobbyDTO;
 import ch.fhnw.projectbois.dto.MessageDTO;
+import ch.fhnw.projectbois.json.JsonUtils;
 import ch.fhnw.projectbois.network.Server;
 import ch.fhnw.projectbois.network.ServerClient;
+
+/**
+ * 
+ * @author Leeroy Koller
+ *
+ */
 
 public class ChatRequestHandler extends RequestHandler {
 
@@ -15,11 +25,27 @@ public class ChatRequestHandler extends RequestHandler {
 	@Override
 	protected void handleRequest() {
 		if(request.getRequestId() == RequestId.CHAT_SEND_MSG) {
-			//TO-DO: send msg;
+			this.sendMessage();
 			//Dummy code
-			MessageDTO message = new MessageDTO();
-			client.getLobby().sendMessage(message);
+			//MessageDTO message = new MessageDTO();
+			//client.getLobby().sendMessage(message);
 		}
+	}
+	
+	private void sendMessage() {
+		String json = request.getJsonDataObject();
+		MessageDTO messageDTO = JsonUtils.Deserialize(json, MessageDTO.class);
+		//logic: make ready to post message
+		
+		// send response to client
+		//lobbyDTO.setId(lobby.getId());
+		//lobbyDTO.addPlayer(client.getUser().getUsername());
+		json = JsonUtils.Serialize(messageDTO);
+		Response response = new Response(ResponseId.RECEIVE_MSG, request.getRequestId(), json);
+
+		client.sendResponse(response);		
+		
+		
 	}
 
 }
