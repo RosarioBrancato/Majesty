a5586d42866cf9c0cf943f77a04ebf41f6c3297e
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-11-26 19:01:51 +0100

    second steps chat

--- MajestyCommons/src/ch/fhnw/projectbois/dto/MessageDTO.java
+++ MajestyCommons/src/ch/fhnw/projectbois/dto/MessageDTO.java
@@ -1,5 +1,35 @@
 package ch.fhnw.projectbois.dto;
 
-public class MessageDTO {
+import ch.fhnw.projectbois.enumerations.ChatMember;
 
+public class MessageDTO {
+	int id;
+	ChatMember receiver;
+	ChatMember author;
+	String message;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ChatMember getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(ChatMember receiver) {
+		this.receiver = receiver;
+	}
+	public ChatMember getAuthor() {
+		return author;
+	}
+	public void setAuthor(ChatMember author) {
+		this.author = author;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
 }
