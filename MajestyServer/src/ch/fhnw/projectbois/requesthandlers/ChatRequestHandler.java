71446696346b0a1fc6dd381a10b1bea787c008c9
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-12-01 22:14:31 +0100

    Chat almost done
    debugging, whisper and system notification works.
    Formatting of notification and whisper follows

--- MajestyServer/src/ch/fhnw/projectbois/requesthandlers/ChatRequestHandler.java
+++ MajestyServer/src/ch/fhnw/projectbois/requesthandlers/ChatRequestHandler.java
@@ -28,53 +28,123 @@ public class ChatRequestHandler extends RequestHandler {
 
 	@Override
 	protected void handleRequest() {
-		if(request.getRequestId() == RequestId.CHAT_SEND_MSG) {
+		if (request.getRequestId() == RequestId.CHAT_SEND_MSG) {
 			this.sendMessage();
-			
+
 		} else {
-			
+
 		}
 	}
-	
+
 	private void sendMessage() {
 		String json = request.getJsonDataObject();
 		MessageDTO messageDTO = JsonUtils.Deserialize(json, MessageDTO.class);
-		
+
+		ChatMember author = messageDTO.getAuthor();
+		ChatMember receiver = messageDTO.getReceiver();
+
 		// send response to client
 		int id = IdFactory.getInstance().getNewId(MessageDTO.class.getName());
 		messageDTO.setId(id);
-		
-		//messageDTO.setAuthor(ChatMember.System); //for now just system. how to check who is the author??
+
+		// messageDTO.setAuthor(ChatMember.System); //for now just system. how to check
+		// who is the author??
 		json = JsonUtils.Serialize(messageDTO);
 		Response response = new Response(ResponseId.RECEIVE_MSG, request.getRequestId(), json);
 
 		Lobby lobby = client.getLobby();
 		ArrayList<ServerClient> clients = lobby.getClients();
-		
-		if (messageDTO.getReceiver() == ChatMember.All) {
-			for(ServerClient c : clients) {
+
+		if (receiver == ChatMember.All) {
+			for (ServerClient c : clients) {
 				c.sendResponse(response);
 			}
+
+		} else {
+			// see own post
+			client.sendResponse(response);
+			if (receiver == ChatMember.Player1) {
+				clients.get(0).sendResponse(response);
+
+			} else if (receiver == ChatMember.Player2) {
+				if (clients.size() <= 2) {
+					clients.get(1).sendResponse(response);
+				}
+
+			} else if (receiver == ChatMember.Player3) {
+				if (clients.size() <= 3) {
+					clients.get(2).sendResponse(response);
+				}
+
+			} else if (receiver == ChatMember.Player4) {
+				if (clients.size() <= 4) {
+					clients.get(3).sendResponse(response);
+				}
+			}
+
+			// System notification
+			MessageDTO whisperMessage = new MessageDTO();
+			id = IdFactory.getInstance().getNewId(MessageDTO.class.getName());
+			whisperMessage.setId(id);
+			whisperMessage.setAuthor(ChatMember.System);
+
+			String notificationMessage = getUsernameByChatmember(author) + " hat zu "
+					+ getUsernameByChatmember(receiver) + " geflüstert.";
+			whisperMessage.setMessage(notificationMessage);
+
+			json = JsonUtils.Serialize(whisperMessage);
+			Response whisperNotification = new Response(ResponseId.RECEIVE_MSG, request.getRequestId(), json);
+
+			if (messageDTO.getAuthor() != ChatMember.Player1 && messageDTO.getReceiver() != ChatMember.Player1) {
+				clients.get(0).sendResponse(whisperNotification);
+
+			} else if (messageDTO.getAuthor() != ChatMember.Player2 && messageDTO.getReceiver() != ChatMember.Player2) {
+				if (clients.size() >= 2) {
+					clients.get(1).sendResponse(whisperNotification);
+				}
+
+			} else if (messageDTO.getAuthor() != ChatMember.Player3 && messageDTO.getReceiver() != ChatMember.Player3) {
+				if (clients.size() >= 3) {
+					clients.get(2).sendResponse(whisperNotification);
+				}
+
+			} else if (messageDTO.getAuthor() != ChatMember.Player4 && messageDTO.getReceiver() != ChatMember.Player4) {
+				if (clients.size() >= 4) {
+					clients.get(3).sendResponse(whisperNotification);
+				}
+			}
 		}
-		
-		// does not work yet
-		//		||
-		//	   \||/
-		//		\/
-		if (messageDTO.getAuthor() == ChatMember.Player1) {
-			//send response to client player1
-			//send notification to all others as "system"
-		} else if (messageDTO.getAuthor() == ChatMember.Player2) {
-			//send response to client player2
-			//send notification to all others as "system"
-		} else if (messageDTO.getAuthor() == ChatMember.Player3) {
-			//send response to client player3
-			//send notification to all others as "system"
-		} else if (messageDTO.getAuthor() == ChatMember.Player4) {
-			//send response to client player4
-			//send notification to all others as "system"
-		} 
-		
+	}
+
+	private String getUsernameByChatmember(ChatMember member) {
+		String username = "";
+		ArrayList<ServerClient> clients = client.getLobby().getClients();
+
+		switch (member) {
+		case Player1:
+			username = clients.get(0).getUser().getUsername();
+			break;
+		case Player2:
+			if (clients.size() >= 2) {
+				username = clients.get(1).getUser().getUsername();
+			}
+			break;
+		case Player3:
+			if (clients.size() >= 3) {
+				username = clients.get(2).getUser().getUsername();
+			}
+			break;
+		case Player4:
+			if (clients.size() >= 4) {
+				username = clients.get(3).getUser().getUsername();
+			}
+			break;
+		default:
+			username = "Unkown";
+			break;
+		}
+
+		return username;
 	}
 
 }
