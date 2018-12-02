da54d28e9ad72f45d6e4a371ce311e40a772a92d
Administrator <Administrator@vmi218554>
2018-12-02 17:41:24 +0100

    Added correct login responses to UI

--- MajestyServer/src/ch/fhnw/projectbois/requesthandlers/AuthRequestHandler.java
+++ MajestyServer/src/ch/fhnw/projectbois/requesthandlers/AuthRequestHandler.java
@@ -13,7 +13,7 @@ import ch.fhnw.projectbois.communication.Response;
 import ch.fhnw.projectbois.communication.ResponseId;
 import ch.fhnw.projectbois.dto.LoginDTO;
 import ch.fhnw.projectbois.dto.UserDTO;
-import ch.fhnw.projectbois.general.IdFactory;
+// import ch.fhnw.projectbois.general.IdFactory;
 import ch.fhnw.projectbois.json.JsonUtils;
 import ch.fhnw.projectbois.network.Server;
 import ch.fhnw.projectbois.network.ServerClient;
@@ -29,9 +29,13 @@ public class AuthRequestHandler extends RequestHandler {
 	@Override
 	protected void handleRequest() {
 		if(request.getRequestId() == RequestId.LOGIN) {
-			int uid;
+			int DBuid = 0;
 			String DBpassword = null;
 			String DBsalt = null;
+			TokenFactory token = null;
+			UserDTO user = null;
+			Response response = null;
+			String json = "";
 			LoginDTO loginRequest = JsonUtils.Deserialize(request.getJsonDataObject(), LoginDTO.class);
 			String getUserString = "SELECT `uid`, `password`, `salt` FROM `user` WHERE `nickname` = ? LIMIT 1;";
 			Connection con = null;
@@ -40,35 +44,34 @@ public class AuthRequestHandler extends RequestHandler {
 			try {
 				con = DbAccess.getConnection();
 				getUser = con.prepareStatement(getUserString);
-				getUser.setString(1, "alex");
+				getUser.setString(1, loginRequest.getUsername());
 				ResultSet dbUsers = getUser.executeQuery();
 				while(dbUsers.next()) {
+					DBuid = dbUsers.getInt(1);
 					DBpassword = dbUsers.getString(2);
 					DBsalt = dbUsers.getString(3);
 				}
 				getUser.close();
 			}catch(SQLException e) {
 				logger.severe("Login failed due to database error: " + e.toString());
+				response = new Response(ResponseId.AUTH_ERROR_SERVER, request.getRequestId(), json);
 			}
 			
-			TokenFactory token = null;
-			UserDTO user = null;
-			Response response = null;
-			String json = "";
-			
-			LoginHandler lh = new LoginHandler();
-			if(lh.checkMatchingPasswords(loginRequest.getPassword(), DBpassword, DBsalt)) {
-				token = TokenFactory.getInstance();
-				uid = IdFactory.getInstance().getNewId(UserDTO.class.getName());
-				user = new UserDTO(uid, loginRequest.getUsername(), token.getNewToken());
-				client.setUser(user);
-				json = JsonUtils.Serialize(user);
-				response = new Response(ResponseId.AUTH_OK, request.getRequestId(), json);
-			}else {
-				response = new Response(ResponseId.AUTH_ERROR_CREDENTIALS, request.getRequestId(), json);
-				logger.severe("Login failed due to invalid user input. Requested User: " + loginRequest.getUsername());
+			if(DBuid != 0 && response == null) {
+				LoginHandler lh = new LoginHandler();
+				if(lh.checkMatchingPasswords(loginRequest.getPassword(), DBpassword, DBsalt)) {
+					token = TokenFactory.getInstance();
+					//uid = IdFactory.getInstance().getNewId(UserDTO.class.getName());
+					user = new UserDTO(DBuid, loginRequest.getUsername(), token.getNewToken());
+					client.setUser(user);
+					json = JsonUtils.Serialize(user);
+					response = new Response(ResponseId.AUTH_OK, request.getRequestId(), json);
+				}else {
+					response = new Response(ResponseId.AUTH_ERROR_CREDENTIALS, request.getRequestId(), json);
+					logger.severe("Login failed due to invalid user input. Requested User: " + loginRequest.getUsername());
+				}
 			}
-			
+
 			client.sendResponse(response);
 		}
 	}
