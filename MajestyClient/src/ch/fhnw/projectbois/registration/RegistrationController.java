c1e26aa94caa9d5cdd8c7c7389ab97a43b965f48
Alexandre Miccoli <alex@micco.li>
2018-12-02 22:15:52 +0100

    Added handling of changed language + registration popup

--- MajestyClient/src/ch/fhnw/projectbois/registration/RegistrationController.java
+++ MajestyClient/src/ch/fhnw/projectbois/registration/RegistrationController.java
@@ -1,11 +1,72 @@
 package ch.fhnw.projectbois.registration;
 
+import ch.fhnw.projectbois._application.MetaContainer;
 import ch.fhnw.projectbois._mvc.Controller;
+import ch.fhnw.projectbois.utils.DialogUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class RegistrationController extends Controller<RegistrationModel, RegistrationView> {
-
+	private Stage stage;
+	
 	public RegistrationController(RegistrationModel model, RegistrationView view) {
 		super(model, view);
+	}
+
+	public void showAndWait() {
+		this.stage = DialogUtils.getStageModal(MetaContainer.getInstance().getMainStage());
+		this.stage.setTitle(translator.getTranslation("lbl_Registration_Title"));
+		//this.stage.initStyle(StageStyle.UNDECORATED);
+		this.stage.setScene(new Scene(this.getViewRoot()));
+		this.stage.showAndWait();
+	}
+	
+	/*@FXML
+	private Label lbl_Registration_username;
+	
+	@FXML
+	private Label lbl_Registration_email;
+	
+	@FXML
+	private Label lbl_Registration_pwd;
+	
+	@FXML
+	private Label lbl_Registration_pwdRepead;
+
+	@FXML
+	private Button btn_Registration_cancel;
+	
+	@FXML
+	private Button btn_Registration_register;
+	
+	@FXML
+	private TextField txt_Registration_username;
+	
+	@FXML
+	private TextField txt_Registration_email;
+	
+	@FXML
+	private PasswordField txt_Registration_pwd;
+	
+	@FXML
+	private PasswordField txt_Registration_pwdRepead;
+	*/
+	
+	@FXML
+	private void btn_Registration_cancelClicked(ActionEvent event) {
+		this.stage.close();
+	}
+	
+	@FXML
+	private void btn_Registration_registerClicked(ActionEvent event) {
+		System.out.println("Register Clicked");
 	}
 
 }
