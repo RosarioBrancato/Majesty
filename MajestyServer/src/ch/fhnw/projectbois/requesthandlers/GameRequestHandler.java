7c03db4beca18d20657fec7db8ae1739aaf092ed
Rosario B <rossa_4057@hotmail.com>
2018-12-05 17:30:36 +0100

    Unexpected Leaving of a Player Is Now Handled
    Games & Lobbies will handle an unexpected leaving of a player:
    - Player closing the program/disconnecting during a game.
    - Player closing the program/disconnecting while in a lobby.

--- MajestyServer/src/ch/fhnw/projectbois/requesthandlers/GameRequestHandler.java
+++ MajestyServer/src/ch/fhnw/projectbois/requesthandlers/GameRequestHandler.java
@@ -8,7 +8,6 @@ import ch.fhnw.projectbois.game.GameLogic;
 import ch.fhnw.projectbois.game.GameStateServer;
 import ch.fhnw.projectbois.gameobjects.GameMove;
 import ch.fhnw.projectbois.gameobjects.GameState;
-import ch.fhnw.projectbois.gameobjects.Player;
 import ch.fhnw.projectbois.general.IdFactory;
 import ch.fhnw.projectbois.json.JsonUtils;
 import ch.fhnw.projectbois.network.Lobby;
@@ -43,43 +42,12 @@ public class GameRequestHandler extends RequestHandler {
 	}
 
 	private void updateGameState() {
-		GameState gameState = client.getLobby().getGameState();
-		String json = JsonUtils.Serialize(gameState);
-
-		Response response = new Response(ResponseId.UPDATE_GAMESTATE, request.getRequestId(), json);
-
 		Lobby lobby = client.getLobby();
-		for (ServerClient c : lobby.getClients()) {
-			c.sendResponse(response);
-		}
-	}
+		if (lobby != null && lobby.isGameStarted()) {
+			GameState gameState = lobby.getGameState();
+			String json = JsonUtils.Serialize(gameState);
 
-	private void doMove() {
-		String json = request.getJsonDataObject();
-		GameMove gameMove = JsonUtils.Deserialize(json, GameMove.class);
-
-		Lobby lobby = client.getLobby();
-		GameState gameState = lobby.getGameState();
-		GameStateServer gameStateServer = lobby.getGameStateServer();
-
-		if (!gameStateServer.isGameEnded()) {
-
-			GameLogic logic = new GameLogic(gameState, gameStateServer);
-
-			logic.executeMove(client.getUser().getUsername(), gameMove);
-			boolean gameOver = logic.startNextTurn();
-
-			Response response;
-			json = JsonUtils.Serialize(gameState);
-
-			if (!gameOver) {
-				response = new Response(ResponseId.UPDATE_GAMESTATE, request.getRequestId(), json);
-
-			} else {
-				logic.endGame();
-
-				response = new Response(ResponseId.GAME_ENDED, request.getRequestId(), json);
-			}
+			Response response = new Response(ResponseId.UPDATE_GAMESTATE, request.getRequestId(), json);
 
 			for (ServerClient c : lobby.getClients()) {
 				c.sendResponse(response);
@@ -87,10 +55,44 @@ public class GameRequestHandler extends RequestHandler {
 		}
 	}
 
+	private void doMove() {
+		Lobby lobby = client.getLobby();
+		if (lobby != null && lobby.isGameStarted()) {
+			String json = request.getJsonDataObject();
+			GameMove gameMove = JsonUtils.Deserialize(json, GameMove.class);
+
+			GameState gameState = lobby.getGameState();
+			GameStateServer gameStateServer = lobby.getGameStateServer();
+
+			if (!gameStateServer.isGameEnded()) {
+
+				GameLogic logic = new GameLogic(gameState, gameStateServer);
+
+				logic.executeMove(client.getUser().getUsername(), gameMove);
+				boolean gameOver = logic.startNextTurn();
+
+				Response response;
+				json = JsonUtils.Serialize(gameState);
+
+				if (!gameOver) {
+					response = new Response(ResponseId.UPDATE_GAMESTATE, request.getRequestId(), json);
+
+				} else {
+					logic.endGame();
+
+					response = new Response(ResponseId.GAME_ENDED, request.getRequestId(), json);
+				}
+
+				for (ServerClient c : lobby.getClients()) {
+					c.sendResponse(response);
+				}
+			}
+		}
+	}
+
 	private void startGame() {
 		Lobby lobby = client.getLobby();
-
-		if (!lobby.isGameStarted()) {
+		if (lobby != null && !lobby.isGameStarted()) {
 			GameState gameState = new GameState();
 			gameState.setId(IdFactory.getInstance().getNewId(GameState.class.getName()));
 			gameState.setCardSideA(lobby.isCardSideA());
@@ -119,38 +121,8 @@ public class GameRequestHandler extends RequestHandler {
 
 	private void leaveGame() {
 		Lobby lobby = client.getLobby();
-		GameState gameState = lobby.getGameState();
-
-		// update game state object
-		Player player = gameState.getBoard().getPlayers().stream()
-				.filter(f -> f.getUsername().equals(client.getUser().getUsername())).findFirst().get();
-
-		GameLogic logic = new GameLogic(lobby.getGameState(), lobby.getGameStateServer());
-		logic.removePlayer(player);
-
-		lobby.getClients().remove(client);
-		int clientsCount = lobby.getClients().size();
-
-		Response response = null;
-		String json = JsonUtils.Serialize(gameState);
-		if (clientsCount > 1) {
-			// message other players about a player leaving
-			response = new Response(ResponseId.GAME_PLAYER_LEFT, request.getRequestId(), json);
-
-		} else if (clientsCount == 1) {
-			// only 1 player left in the lobby -> game ended
-			response = new Response(ResponseId.GAME_ENDED, request.getRequestId(), json);
-		}
-
-		if (response != null) {
-			for (ServerClient c : lobby.getClients()) {
-				c.sendResponse(response);
-			}
-		}
-
-		// last player left the lobby -> remove lobby
-		if (lobby.getClients().size() >= 0) {
-			server.getLobbies().remove(lobby);
+		if (lobby != null && lobby.isGameStarted()) {
+			lobby.removeClient(client);
 		}
 	}
 
