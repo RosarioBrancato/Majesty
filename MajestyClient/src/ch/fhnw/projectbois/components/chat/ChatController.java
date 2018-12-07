bd4b60a2d84e778199bc3de965f41574575802ae
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-12-07 22:10:59 +0100

    minor enhancements on chat
    -close and open chat works now
    -send with enter key

--- MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatController.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatController.java
@@ -8,6 +8,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * 
@@ -22,13 +27,21 @@ public class ChatController extends Controller<ChatModel, ChatView> {
 	private TextArea txtChat;
 
 	@FXML
-	private TextArea txtMessage;
+	private TextField txtMessage;
 
 	@FXML
 	private Button btnSend;
 
 	@FXML
+	private ImageView imgSend;
+
+	@FXML
 	private Button btnClose;
+
+	@FXML
+	private AnchorPane pnlRoot;
+	
+	private boolean isClosed = false;
 
 	public ChatController(ChatModel model, ChatView view) {
 		super(model, view);
@@ -61,38 +74,73 @@ public class ChatController extends Controller<ChatModel, ChatView> {
 	@FXML
 	private void btnSend_Click(ActionEvent event) {
 		MessageDTO message = new MessageDTO();
+		
+		if(!txtMessage.getText().isEmpty()) {
+			message.setMessage(txtMessage.getText());
+			message.setAuthor(model.getCurrentUserChatMember());
 
-		message.setMessage(txtMessage.getText());
-		message.setAuthor(model.getCurrentUserChatMember());
+			// check if whispering and set receiver
+			String messageText = message.getMessage();
+			String[] parts = messageText.split(":");
+			String part1 = parts[0];
 
-		// check if whispering and set receiver
-		String messageText = message.getMessage();
-		String[] parts = messageText.split(":");
-		String part1 = parts[0];
+			ChatMember receiver = model.getUsernameMap().get(part1);
+			System.out.println(receiver);
+			if (receiver != null && receiver != model.getCurrentUserChatMember()) {
+				String newMessage = "";
+				for (int i = 1; i < parts.length; i++) {
+					newMessage += parts[i];
 
-		ChatMember receiver = model.getUsernameMap().get(part1);
-		if (receiver != null && receiver != model.getCurrentUserChatMember()) {
-			String newMessage = "";
-			for (int i = 1; i < parts.length; i++) {
-				newMessage += parts[i];
-
-				if (i + 1 < parts.length) {
-					newMessage += ":";
+					if (i + 1 < parts.length) {
+						newMessage += ":";
+					}
 				}
+				message.setMessage(newMessage);
+				message.setReceiver(receiver);
+			} else {
+				message.setReceiver(ChatMember.All);
 			}
-			message.setMessage(newMessage);
-			message.setReceiver(receiver);
-		} else {
-			message.setReceiver(ChatMember.All);
-		}
 
-		model.sendMessage(message);
+			model.sendMessage(message);
+		}
+		
 		txtMessage.clear();
 	}
 
 	@FXML
+	private void onEnterKey(KeyEvent e) {
+		KeyCode key = e.getCode();
+		if (key == KeyCode.ENTER) {
+			btnSend_Click(new ActionEvent());
+		}
+	}
+	
+	
+	@FXML
 	private void btnMinimize_Click(ActionEvent event) {
-		this.getViewRoot().setVisible(false);
+
+		if (isClosed == false) {
+			txtChat.setVisible(false);
+			txtMessage.setVisible(false);
+			btnSend.setVisible(false);
+			imgSend.setVisible(false);
+
+			pnlRoot.setMinHeight(20);
+			pnlRoot.setMaxHeight(20);
+			pnlRoot.setPrefHeight(20);
+			isClosed = true;
+		} else if (isClosed == true) {
+			txtChat.setVisible(true);
+			txtMessage.setVisible(true);
+			btnSend.setVisible(true);
+			imgSend.setVisible(true);
+
+			//match prefHeight to window size
+			pnlRoot.setMaxHeight(1000);
+			pnlRoot.setPrefHeight(1000);
+			isClosed = false;
+		}
+
 	}
 
 }
