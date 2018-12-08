9b3d0adc0497b4a2db69760b0521ecca6ac8a2b2
Dario Stöckli <dario.stoeckli@students.fhnw.ch>
2018-12-08 07:22:35 +0100

    Added Manual section (French and Italian PDF missing)

--- MajestyClient/src/ch/fhnw/projectbois/components/menubar/MenuBarModel.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/menubar/MenuBarModel.java
@@ -1,7 +1,29 @@
 package ch.fhnw.projectbois.components.menubar;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
 import ch.fhnw.projectbois._mvc.Model;
 
 public class MenuBarModel extends Model {
 
+	public void openManual() {
+		if (Desktop.isDesktopSupported()) {
+			File manual;
+			//Open Manual in corresponding language, if Manual in language not available open in English
+		    try {
+		    	if (translator.getLocale().equals(Locale.GERMAN)) {
+		    		manual = new File("resources/manuals/majesty_ger_rules_lowres.pdf");
+		    	} else {
+		    		manual = new File("resources/manuals/majesty_eng_rules_lowres.pdf");
+		    	}
+		    	Desktop.getDesktop().open(manual);
+		    } catch (IOException ex) {
+		        //No PDF application available to open file with, thus no action
+		    }
+		}
+
+	}
 }
