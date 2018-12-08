6d255b004b36758cc70a766ec9bc1bfd474c0bf0
Rosario B <rossa_4057@hotmail.com>
2018-12-08 13:38:50 +0100

    System Chat Messages Can Now Be Multi Lang. + Whisper Bug Fix

--- MajestyCommons/src/ch/fhnw/projectbois/dto/MessageDTO.java
+++ MajestyCommons/src/ch/fhnw/projectbois/dto/MessageDTO.java
@@ -1,35 +1,69 @@
 package ch.fhnw.projectbois.dto;
 
+import java.util.ArrayList;
+
 import ch.fhnw.projectbois.enumerations.ChatMember;
 
 public class MessageDTO {
-	int id;
-	ChatMember receiver;
-	ChatMember author;
-	String message;
+
+	private int id;
+	private ChatMember receiver;
+	private ChatMember author;
+	private String message;
+
+	private String translationKey;
+	private ArrayList<String> formatVariables;
+
+	public MessageDTO() {
+		this.formatVariables = new ArrayList<>();
+	}
+	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public ChatMember getReceiver() {
 		return receiver;
 	}
+
 	public void setReceiver(ChatMember receiver) {
 		this.receiver = receiver;
 	}
+
 	public ChatMember getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(ChatMember author) {
 		this.author = author;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
+
+	public String getTranslationKey() {
+		return translationKey;
+	}
+
+	public void setTranslationKey(String translationKey) {
+		this.translationKey = translationKey;
+	}
+
+	public ArrayList<String> getFormatVariables() {
+		return formatVariables;
+	}
+
+	public void setFormatVariables(ArrayList<String> formatVariables) {
+		this.formatVariables = formatVariables;
+	}
+
 }
