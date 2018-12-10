4f7c87684d9ac3058d2e138b05c934c47d449e1d
Leeeeroy <leeroy.koller@students.fhnw.ch>
2018-12-10 19:21:15 +0100

    chat minor updates 3
    -notification bell works now
    -chat height changed to 300 in game

--- MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatView.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/chat/ChatView.java
@@ -5,14 +5,29 @@ import java.net.URL;
 import ch.fhnw.projectbois._mvc.View;
 
 public class ChatView extends View<ChatModel> {
+	
+	public static final double PREF_HEIGHT = 300;
 
+	private double prefHeightOpen = 0;
+	
+	
 	public ChatView(ChatModel model) {
 		super(model);
+		
+		this.setPrefHeightOpen(PREF_HEIGHT);
 	}
 
 	@Override
 	protected URL getFXML() {
 		return this.getClass().getResource("ChatView.fxml");
+	}
+
+	public double getPrefHeightOpen() {
+		return prefHeightOpen;
+	}
+
+	public void setPrefHeightOpen(double prefHeightOpen) {
+		this.prefHeightOpen = prefHeightOpen;
 	}
 
 }
