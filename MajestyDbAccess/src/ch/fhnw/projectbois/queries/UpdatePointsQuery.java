6645f5306616b981474bc0b8472034c140fa3bbf
Dario Stöckli <dario.stoeckli@students.fhnw.ch>
2018-12-10 20:45:43 +0100

    Game Ending - Points get written to DB

--- MajestyDbAccess/src/ch/fhnw/projectbois/queries/UpdatePointsQuery.java
+++ MajestyDbAccess/src/ch/fhnw/projectbois/queries/UpdatePointsQuery.java
@@ -1,31 +1,34 @@
 package ch.fhnw.projectbois.queries;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 import ch.fhnw.projectbois.access.DbAccess;
-import ch.fhnw.projectbois.dto.UserDTO;
+import ch.fhnw.projectbois.gameobjects.Player;
+import ch.fhnw.projectbois.log.LoggerFactory;
 
 public class UpdatePointsQuery {
 	
-	public boolean setPoints(UserDTO user) {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	public boolean setPoints(ArrayList<Player> players) {
 		 boolean success = false;
 		 try {
 			 Connection connection = DbAccess.getConnection();
-			 Statement statement = connection.createStatement();
-			 ResultSet rs;
-      
-
-			// rs = statement.executeQuery("UPDATE user SET points = points+10 WHERE nickname="dario"");
-			 //while ( rs.next() ) {
-				 
-			 //}
+			 PreparedStatement preparedstatement = connection.prepareStatement("UPDATE user SET points=points+? WHERE nickname=?");
+			 int rs;
 			 
+			 for (Player player : players) {
+				 preparedstatement.setInt(1, player.getPoints());
+				 preparedstatement.setString(2, player.getUsername());
+				 rs = preparedstatement.executeUpdate();
+				logger.info("Wrote score to db: " + player.getUsername() + " made " + player.getPoints() + " - rows affected: " + rs); 
+			 }
 			 success=true;
         connection.close();
 		 } catch (Exception e) {
-			 System.err.println("Got an exception while querying Database!");
+			 System.err.println("Got an exception while writing points to Database!");
 			 System.err.println(e.getMessage());
 		 }
 	return success;
