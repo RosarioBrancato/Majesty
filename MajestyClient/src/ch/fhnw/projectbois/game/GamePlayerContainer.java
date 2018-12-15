f3d6ec5f356ece485388b2ec9dca8e198efcc1b2
Rosario B <rossa_4057@hotmail.com>
2018-12-15 17:45:19 +0100

    Game - New Gameboard Partial Solution

--- MajestyClient/src/ch/fhnw/projectbois/game/GamePlayerContainer.java
+++ MajestyClient/src/ch/fhnw/projectbois/game/GamePlayerContainer.java
@@ -1,19 +1,14 @@
 package ch.fhnw.projectbois.game;
 
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
 
 public class GamePlayerContainer {
 
 	private String username = null;
 
-	private GridPane pnlCards = null;
-	private Label lblInfo = null;
-	private Label[] locationCardCount = null;
-	
-	public GamePlayerContainer() {
-		this.locationCardCount = new Label[8];
-	}
+	private Label lblUsername = null;
+	private Label lblPoints = null;
+	private Label lblMeeples = null;
 
 	public String getUsername() {
 		return username;
@@ -23,28 +18,28 @@ public class GamePlayerContainer {
 		this.username = username;
 	}
 
-	public GridPane getPnlCards() {
-		return pnlCards;
+	public Label getLblUsername() {
+		return lblUsername;
 	}
 
-	public void setPnlCards(GridPane pnlCards) {
-		this.pnlCards = pnlCards;
+	public void setLblUsername(Label lblUsername) {
+		this.lblUsername = lblUsername;
 	}
 
-	public Label getLblInfo() {
-		return lblInfo;
+	public Label getLblPoints() {
+		return lblPoints;
 	}
 
-	public void setLblInfo(Label lblInfo) {
-		this.lblInfo = lblInfo;
+	public void setLblPoints(Label lblPoints) {
+		this.lblPoints = lblPoints;
 	}
 
-	public Label getLabelCardCountByIndex(int index) {
-		return this.locationCardCount[index];
+	public Label getLblMeeples() {
+		return lblMeeples;
 	}
-	
-	public void setLabelCardCountByIndex(int index, Label label) {
-		this.locationCardCount[index] = label;
+
+	public void setLblMeeples(Label lblMeeples) {
+		this.lblMeeples = lblMeeples;
 	}
 
 }
--- MajestyClient/src/ch/fhnw/projectbois/game/GameView.java
+++ MajestyClient/src/ch/fhnw/projectbois/game/GameView.java
@@ -5,44 +5,32 @@ import java.util.ArrayList;
 
 import ch.fhnw.projectbois._mvc.Controller;
 import ch.fhnw.projectbois._mvc.View;
-import ch.fhnw.projectbois.components.Component;
-import ch.fhnw.projectbois.components.ComponentLoader;
 import ch.fhnw.projectbois.components.chat.ChatController;
 import ch.fhnw.projectbois.components.chat.ChatModel;
 import ch.fhnw.projectbois.components.chat.ChatView;
-import ch.fhnw.projectbois.fxml.FXMLUtils;
 import ch.fhnw.projectbois.gameobjects.GameState;
-import ch.fhnw.projectbois.gameobjects.Location;
 import ch.fhnw.projectbois.gameobjects.Player;
 import ch.fhnw.projectbois.session.Session;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
 public class GameView extends View<GameModel> {
 
-	private GameResourceHelper resourceHelper = null;
 	private GamePlayerContainer playerContainer = null;
 	private ArrayList<GamePlayerContainer> opponentContainers = null;
 
 	public GameView(GameModel model) {
 		super(model);
 
-		this.resourceHelper = new GameResourceHelper();
 		this.opponentContainers = new ArrayList<>();
 	}
 
 	@Override
 	protected URL getFXML() {
-		return this.getClass().getResource("GameView.fxml");
+		return this.getClass().getResource("GameView2.fxml");
 	}
 
 	@Override
@@ -68,12 +56,6 @@ public class GameView extends View<GameModel> {
 
 		// Set gridpane as new root
 		this.root = stackpane;
-
-		// Load CSS
-		stackpane.setStyle("-fx-background-color: #CCFF99");
-		chat.setStyle("-fx-background-color: yellow; -fx-width: 100px;");
-		String displayCSS = this.getClass().getResource("GameView.css").toExternalForm();
-		this.root.getStylesheets().add(displayCSS);
 	}
 
 	public void initGamePlayerContainers() {
@@ -85,110 +67,40 @@ public class GameView extends View<GameModel> {
 		Player p = players.stream().filter(f -> f.getUsername().equals(Session.getCurrentUsername())).findFirst().get();
 		this.playerContainer = new GamePlayerContainer();
 		this.playerContainer.setUsername(p.getUsername());
-		this.playerContainer.setPnlCards((GridPane) this.root.lookup("pnlPlayer1Cards"));
-		this.playerContainer.setLblInfo((Label) this.root.lookup("lblPlayerInfo"));
+		this.playerContainer.setLblUsername((Label) this.root.lookup("lblName1"));
+		this.playerContainer.setLblPoints((Label) this.root.lookup("lblPoints1"));
+		this.playerContainer.setLblMeeples((Label) this.root.lookup("lblMeeples1"));
 
 		// turn clockwise -> player to left after current player
-		int opponentIndex = 0;
+		int opponentNr = 2;
 		for (int i = playerIndex + 1; i < players.size(); i++) {
 			Player player = players.get(i);
-			this.initGamePlayerContainerOpponent(opponentIndex, player);
-			opponentIndex++;
+			this.initGamePlayerContainerOpponent(opponentNr, player);
+			opponentNr++;
 		}
 
 		for (int i = 0; i < playerIndex; i++) {
 			Player player = players.get(i);
-			this.initGamePlayerContainerOpponent(opponentIndex, player);
-			opponentIndex++;
+			this.initGamePlayerContainerOpponent(opponentNr, player);
+			opponentNr++;
 		}
 	}
 
-	public void initGamePlayerContainerOpponent(int index, Player player) {
-		GridPane pnlOpponents = (GridPane) this.root.lookup("pnlOpponents");
-
+	public void initGamePlayerContainerOpponent(int opponentNr, Player player) {
 		GamePlayerContainer container = new GamePlayerContainer();
 		container.setUsername(player.getUsername());
-
-		URL url = ComponentLoader.getResource(Component.PlayerField);
-		GridPane gridPane = (GridPane) FXMLUtils.loadFXML(url);
-		Label lblInfo = (Label) gridPane.getChildren().get(0);
-
-		container.setPnlCards(gridPane);
-		container.setLblInfo(lblInfo);
+		container.setLblUsername((Label) this.root.lookup("lblName" + opponentNr));
+		container.setLblPoints((Label) this.root.lookup("lblPoints" + opponentNr));
+		container.setLblMeeples((Label) this.root.lookup("lblMeeples" + opponentNr));
+		
 		this.opponentContainers.add(container);
-
-		Integer i = new Integer(index);
-		Platform.runLater(() -> {
-			pnlOpponents.add(gridPane, i, 0);
-		});
 	}
 	
 	public void initLocations() {
-		int playersCount = model.getGameState().getBoard().getPlayers().size();
-		GridPane pnlOpponents = (GridPane)this.root.lookup("pnlOpponents");
-		
-		Platform.runLater(() -> {
-			if (playersCount == 3) {
-				pnlOpponents.getColumnConstraints().remove(2);
-				pnlOpponents.getColumnConstraints().get(0).setPercentWidth(50);
-				pnlOpponents.getColumnConstraints().get(1).setPercentWidth(50);
-
-			} else if (playersCount == 2) {
-				pnlOpponents.getColumnConstraints().remove(2);
-				pnlOpponents.getColumnConstraints().remove(1);
-				pnlOpponents.getColumnConstraints().get(0).setPercentWidth(100);
-			}
-		});
-
-		this.fillGamePlayerComponent(this.playerContainer, true);
-
-		for (GamePlayerContainer opponent : this.opponentContainers) {
-			this.fillGamePlayerComponent(opponent, false);
-		}
-	}
-
-	private void fillGamePlayerComponent(GamePlayerContainer container, boolean isPlayer) {
-		GameState gameState = model.getGameState();
-		
-		int playersCount = gameState.getBoard().getPlayers().size();
-
-		int sideChange = 0;
-		if (!gameState.isCardSideA()) {
-			sideChange = Location.OFFSET_B;
-		}
-
-		for (int i = 0; i < 8; i++) {
-
-			ImageView imgLocation = new ImageView();
-			imgLocation.setPreserveRatio(true);
-
-			if (isPlayer) {
-				imgLocation.setFitHeight(175);
-			} else if (playersCount == 2) {
-				imgLocation.setFitHeight(150);
-			} else if (playersCount == 3) {
-				imgLocation.setFitWidth(70);
-			} else if (playersCount == 4) {
-				imgLocation.setFitWidth(45);
-			}
-
-			Image image = resourceHelper.getLocationImage(i + sideChange);
-			imgLocation.setImage(image);
-
-			BorderPane borderPane = new BorderPane(imgLocation);
-			Label label = new Label(translator.getTranslation("lbl_Cards", 0));
-
-			container.setLabelCardCountByIndex(i, label);
-
-			VBox box = new VBox();
-			box.setAlignment(Pos.CENTER);
-			box.getChildren().add(label);
-			borderPane.setBottom(box);
-
-			Integer col = new Integer(i);
-			Platform.runLater(() -> {
-				container.getPnlCards().add(borderPane, col, 0);
-			});
+		if(model.getGameState().isCardSideA()) {
+			this.root.getStylesheets().add("CSSA");
+		} else {
+			this.root.getStylesheets().add("CSS_B");
 		}
 	}
 
