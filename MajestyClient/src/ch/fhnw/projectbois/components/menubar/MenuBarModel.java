964747534febfd4fb75fe4fa35fa6325a8421c5b
Rosario B <Strather@users.noreply.github.com>
2018-12-20 23:40:31 +0100

    External Resources Management

--- MajestyClient/src/ch/fhnw/projectbois/components/menubar/MenuBarModel.java
+++ MajestyClient/src/ch/fhnw/projectbois/components/menubar/MenuBarModel.java
@@ -4,11 +4,13 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
+import java.util.logging.Level;
 
 import ch.fhnw.projectbois._mvc.Model;
 
 /**
  * The Class MenuBarModel.
+ * 
  * @author Dario Stoeckli
  */
 public class MenuBarModel extends Model {
@@ -19,17 +21,19 @@ public class MenuBarModel extends Model {
 	public void openManual() {
 		if (Desktop.isDesktopSupported()) {
 			File manual;
-			//Open Manual in corresponding language, if Manual in language not available open in English
-		    try {
-		    	if (translator.getLocale().equals(Locale.GERMAN)) {
-		    		manual = new File("resources/manuals/majesty_ger_rules_lowres.pdf");
-		    	} else {
-		    		manual = new File("resources/manuals/majesty_eng_rules_lowres.pdf");
-		    	}
-		    	Desktop.getDesktop().open(manual);
-		    } catch (IOException ex) {
-		        //No PDF application available to open file with, thus no action
-		    }
+			// Open Manual in corresponding language, if Manual in language not available
+			// open in English
+			try {
+				if (translator.getLocale().equals(Locale.GERMAN)) {
+					manual = new File("files/manuals/majesty_ger_rules_lowres.pdf");
+				} else {
+					manual = new File("files/manuals/majesty_eng_rules_lowres.pdf");
+				}
+				Desktop.getDesktop().open(manual);
+
+			} catch (IOException ex) {
+				logger.log(Level.SEVERE, "MenuBarModel.openManual()", ex);
+			}
 		}
 
 	}
